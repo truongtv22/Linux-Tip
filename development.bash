5982a67cfa8adffe467e43b20643490a3a879cbf
Tạ Văn Trường <truongtv22@gmail.com>
2017-05-06 23:31:04 +0700

    Create development.bash

---
+++ development.bash
@@ -0,0 +1,26 @@
+# Install Java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# Setting the JAVA_HOME Environment Variable
+sudo update-alternatives --config java # copy path
+sudo nano /etc/environment
+JAVA_HOME="/usr/lib/jvm/java-8-oracle" # add the end of this file, save and exit the file
+source /etc/environment # reload it
+echo $JAVA_HOME # test whether the environment variable
+
+# Install script nvm (Node Version Manager) using cURL
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash # https://github.com/creationix/nvm
+nvm ls-remote # list versions Node.js
+nvm install v6.10.3 # install last version Node.js
+
+# Install Git last version
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
+
+# Setting the ANDOID_HOME && Android SDK Environment Variable
+# ~/.bashrc
+export ANDROID_HOME="/mnt/0436A97F36A9727A/android-sdk-linux"
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
