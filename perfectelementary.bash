de263c4870dc010bcd97614042969453b571c570
Tạ Văn Trường <truongtv22@gmail.com>
2017-05-06 17:28:47 +0700

    Update perfectelementary.bash

--- perfectelementary.bash
+++ perfectelementary.bash
@@ -3,5 +3,14 @@
 # Download Elementary OS from here: 
 # https://elementary.io/
 
+# First you update your system
+sudo apt-get update && sudo apt-get dist-upgrade
+
+# Install GDebi
+sudo apt-get install gdebi
+
 # Install Debian package files (.deb files)
 sudo gdebi *.deb
+
+# Install Gnome Software
+sudo apt install gnome-software
