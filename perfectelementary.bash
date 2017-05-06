e4a1eb43f957048df606ab106c24a4a084ae7bed
Tạ Văn Trường <truongtv22@gmail.com>
2017-05-06 18:32:44 +0700

    Update perfectelementary.bash

--- perfectelementary.bash
+++ perfectelementary.bash
@@ -6,11 +6,33 @@
 # First you update your system
 sudo apt-get update && sudo apt-get dist-upgrade
 
-# Install GDebi
-sudo apt-get install gdebi
-
-# Install Debian package files (.deb files)
-sudo gdebi *.deb
-
 # Install Gnome Software
 sudo apt install gnome-software
+
+# Install GDebi && Install Debian package files (.deb files)
+sudo apt-get install gdebi
+sudo gdebi *.deb
+
+# Install Java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# Setting the JAVA_HOME Environment Variable
+sudo update-alternatives --config java # copy path
+sudo nano /etc/environment
+JAVA_HOME="/usr/lib/jvm/java-8-oracle" # add the end of this file, save and exit the file
+source /etc/environment # reload it
+echo $JAVA_HOME # test whether the environment variable
+
+# Install Git last version
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
+
+# Install Gnome System Monitor
+sudo apt-get update
+sudo apt-get install gnome-system-monitor
+
+# Uninstall package
+sudo apt-get --purge remove ...
