bb6bd7e2050aed5862f884774a0bbd1d1170a343
Tạ Văn Trường <truongtv22@gmail.com>
2017-05-07 17:52:13 +0700

    Update and rename custom icon to applications menu.bash to add application to menu.bash

--- add application to menu.bash
+++ add application to menu.bash
@@ -1,11 +1,9 @@
 # Add custom application to "Applications" menu in Elementary OS
 https://devkeep.wordpress.com/2014/02/17/add-custom-application-to-applications-menu-in-elementary-os/
 
-# Open Terminal create empty android-studio.desktop file
+# Creating the File
 cd /usr/share/applications/
 sudo touch android-studio.desktop
-
-# Edit file just created
 sudo nano android-studio.desktop
 
 # Paste content
@@ -19,3 +17,22 @@ Terminal=false
 Type=Application
 Categories=GNOME;Application;Development;
 StartupNotify=true
+
+# Add Wine Programs to Menu in Linux
+http://geekthis.net/post/add-wine-programs-to-menu-in-linux/
+
+# Creating the File
+cd /usr/share/applications
+sudo touch wine-programname.desktop
+sudo nano wine-programname.desktop
+
+# Paste content
+[Desktop Entry]
+Encoding=UTF-8
+Name=The Programs Name
+Comment=Information About The Program
+Exec=wine "c:\full\path\to\the\program.exe"
+Icon=wine-program
+Terminal=false
+Type=Application
+Categories=Wine-Programs-Accessories;
